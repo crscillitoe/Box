1852caec25af337efcb9fc6e0051fa112bf0d77c
Christian Scillitoe <christian.scillitoe@gmail.com>
2021-10-02 05:21:41 +0000

    Box

--- src/app/game/tile.ts
+++ src/app/game/tile.ts
@@ -4,12 +4,22 @@ import { Coordinate, Player } from "./player";
 export interface Tile {
   getColor(): string;
   getLocation(): Coordinate;
+  /**
   // Could result in side effects by design
+  */
   handleMove(
     player: Player,
     board: Board,
     targetLocation: Coordinate
   ): Coordinate | null;
 
+  /**
+  // Might have side effects
+  // Might not
+  // Don't count on either
+  // Or you'll regret it
+  //
+  // This is by design
+  */
   onTick(player: Player, board: Board): void;
 }
